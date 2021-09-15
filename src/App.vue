1a4487f962c57c10f705bb41c7815843cbb9def3
jackluotong@outlook.com <jackluotong@outlook.com>
2021-09-16 01:18:24 +0800

    init project.

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<!--
+ * @Description: 
+ * @version: 1.0.0
+ * @Author: william
+ * @Date: 2021-09-16 00:28:09
+ * @LastEditors: william
+ * @LastEditTime: 2021-09-16 00:35:36
+ * @For What?: 
+-->
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+    <img alt="Vue logo" src="./assets/logo.png" /><HelloWorld
+        msg="Hello Vue 3 + TypeScript + Vite"
+        testNumber="1"
+    ></HelloWorld>
+</template>
+
+<style>
+#app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+}
+</style>

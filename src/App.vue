c110fb04995b75b1fd96ee6023e08d1c292b329a
williamtong <jackluotong@outlook.com>
2021-10-19 17:03:20 +0800

    lifecycle

--- src/App.vue
+++ src/App.vue
@@ -3,14 +3,19 @@
  * @version: 1.0.0
  * @Author: william
  * @Date: 2021-09-16 00:28:09
- * @LastEditors: Please set LastEditors
- * @LastEditTime: 2021-10-08 18:05:54
+ * @LastEditors: william
+ * @LastEditTime: 2021-10-19 17:01:39
  * @For What?: 
 -->
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import { onMounted } from 'vue'
 import HelloWorld from './components/HelloWorld.vue'
+const day: any = 1
+onMounted(() => {
+    console.log(onMounted)
+})
 </script>
 
 <template>
